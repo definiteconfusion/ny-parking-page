d8ddda3e08f130bbab8f67f117aed7275475dfde
definiteconfusion <122401194+definiteconfusion@users.noreply.github.com>
2025-03-11 10:42:54 -0400

    'init commit'

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Trustbar from "./lib/trustbar.svelte";
+  import { onMount } from "svelte";
+</script>
+
+<main
+  class="main-container"
+>
+  <header class="offical-header-container">
+      <Trustbar />
+  </header>
+  <header class="main-header-container">
+      <div class="main-header-state-logo-container">
+        <img src="src/assets/nys-logo.png" alt="New York State Logo" class="main-header-logo" />
+        <p class="main-header-title">Office of Finance</p>
+      </div>
+  </header>
+  <div class="main-content-container">
+    <h1
+      class="main-content-title"
+    ><b>Dispute a Ticket</b></h1>
+    <div class="main-content-dispute-options-container">
+      <a href="https://i.imgflip.com/51ygax.jpg"><div class="main-content-dispute-option">
+        Online
+      </div></a>
+      <a href="https://i.imgflip.com/51ygax.jpg"><div class="main-content-dispute-option">
+        In-Person
+      </div></a>
+      <a href="https://i.imgflip.com/51ygax.jpg"><div class="main-content-dispute-option">
+        By Mail
+      </div></a>
+      <a href="https://i.imgflip.com/51ygax.jpg"><div class="main-content-dispute-option">
+        Not Guilty
+      </div></a>
+    </div>
+    <h1
+      class="main-content-title"
+    ><b>Important Information</b></h1>
+    <p
+      class="main-content-text"
+    >You must dispute a ticket within 30 days of the date the ticket was issued. If you do not dispute the ticket within 30 days, you may be found guilty by default and <b>additional penalties may apply.</b></p>
+  </div>
+</main>
---
+++ src/lib/trustbar.svelte
@@ -0,0 +1,81 @@
+<script>
+  import { onMount } from "svelte";
+  let showDropdown = false;
+  
+  function toggleDropdown() {
+      showDropdown = !showDropdown;
+  }
+</script>
+
+<style>
+  .header {
+      color: white;
+      padding: 10px 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      font-family: "Montserrat", sans-serif;
+      font-size: 0.8rem;
+
+  }
+
+  .dropdown {
+      display: flex;
+      flex-direction: row;
+      gap: 10px;
+      padding: 10px;
+      border: 1px solid #ddd;
+      background-color: #fff;
+      font-family: "Montserrat", sans-serif;
+      position: absolute;
+      top: 50px;
+      left: 19%;
+  }
+
+  .dropdown-content {
+      padding: 10px;
+      background-color: #fff;
+      border-radius: 5px;
+      max-width: 20rem;
+  }
+
+  .clickable {
+      cursor: pointer;
+      user-select: none;
+      text-decoration: underline;
+      color: #a2a2ff;
+      margin-left: 1.25rem;
+  }
+
+  .dropdown-content-image{
+      width: 50px;
+      height: 50px;
+  }
+</style>
+
+<div class="header">
+  <p>An official website of New York State</p>
+  <div class="clickable" on:click={toggleDropdown}>
+    Here’s how you know ▾
+  </div>
+</div>
+
+{#if showDropdown}
+  <div class="dropdown">
+      <div class="dropdown-content-item">
+        <img src="src/assets/assured_workload.svg" class="dropdown-content-image" alt="Assured Workload" />
+        <div class="dropdown-content">
+          <b>Official websites use .org</b>
+          <p>A .org website belongs to a government organization or to a related sponsored organization.</p>
+      </div>
+    </div>
+    <div class="dropdown-content-item">
+      <img src="src/assets/vpn_lock.svg" class="dropdown-content-image" alt="Assured Workload" />
+      <div class="dropdown-content">
+        <b>Secure ny.org websites use HTTPS</b>
+        <p>A lock icon or https:// means you’ve safely connected to a ny.org website. Share sensitive information only on official, secure websites</p>
+    </div>
+  </div>
+  </div>
+{/if}
+
